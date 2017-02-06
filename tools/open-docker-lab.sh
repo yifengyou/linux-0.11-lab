dcafc34176594e0d82ae4e9551563459d06e4bb1
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    tools: Create desktop shotcut
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/open-docker-lab.sh
+++ tools/open-docker-lab.sh
@@ -10,4 +10,10 @@ pwd=ubuntu
 
 which $browser 2>&1>/dev/null \
     && ($browser $url 2>&1>/dev/null &) \
-    && echo "Please login $url with password: $pwd" && exit 0
+    && echo "Please login $url with password: $pwd"
+
+
+# Create local shotcut on  Desktop
+echo '#!/usr/bin/env xdg-open' >  ~/Desktop/linux-0.11-lab.desktop
+cat $TOP_DIR/linux-lab.desktop | sed "s%Exec=.*%Exec=$browser $url 2>\&1>/dev/null \&%g" | sed "s%lxterminal.xpm%chromium-browser.png%g">> ~/Desktop/linux-0.11-lab.desktop
+chmod a+x ~/Desktop/linux-0.11-lab.desktop
