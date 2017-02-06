637ed5b196662b7d3cd7fa42d166c78fb4e5dc5a
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    tools: Fix up ubuntu version get script
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/install-docker-lab.sh
+++ tools/install-docker-lab.sh
@@ -10,7 +10,7 @@ TOP_DIR=$(dirname `readlink -f $0`)
 which docker
 if [ $? -eq 1 ]; then
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    version=`sed -n -e "/main/p" /etc/apt/sources.list | head -1 | cut -d' ' -f3`
+    version=`sed -n -e "/ main/p" /etc/apt/sources.list | grep -v ^# | head -1 | cut -d' ' -f3`
     echo "deb https://apt.dockerproject.org/repo ubuntu-${version} main" > /etc/apt/sources.list.d/docker.list
     apt-get -y update
     apt-get -y install docker-engine
@@ -22,4 +22,3 @@ if [ $? -eq 1 ]; then
 fi
 
 docker build -t $image_name $TOP_DIR/../
-
