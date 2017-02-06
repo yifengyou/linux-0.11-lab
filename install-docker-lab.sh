332f62359c2ae74be7f2ecffae7abbd69ce15aa3
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    script: Add two scripts to easier the lab usage
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ install-docker-lab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# need to run with sudo
+#
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+version=`sed -n -e "/main$/p" /etc/apt/sources.list | head -1 | cut -d' ' -f3`
+echo "deb https://apt.dockerproject.org/repo ubuntu-$(version) main" > /etc/apt/sources.list.d/docker.list
+apt-get -y update
+apt-get -y install docker-engine
+usermod -aG docker $USER
+
+docker build -t tinylab/linux-0.11-lab ./
