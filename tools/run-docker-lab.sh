f83b83f67a7f43135c06b5004cb2c1baf9b17ee3
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    tools: Generate dynamic vnc ports to allow multiple vms
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/run-docker-lab.sh
+++ tools/run-docker-lab.sh
@@ -3,21 +3,26 @@
 # start the linux 0.11 lab via docker with the shared source in $PWD/
 #
 
+TOP_DIR=$(dirname `readlink -f $0`)
+
 docker_image=tinylab/linux-0.11-lab
-local_lab_dir=$PWD/
+local_lab_dir=`dirname $TOP_DIR`
 remote_lab_dir=/linux-0.11-lab/
 
 browser=chromium-browser
-port=6080
-url=http://localhost:$port/vnc.html
+remote_port=6080
+local_port=$((RANDOM/500+6080))
+url=http://localhost:$local_port/vnc.html
 pwd=ubuntu
 
-CONTAINER_ID=$(docker run -d -p $port:$port -v $local_lab_dir:$remote_lab_dir $docker_image)
+CONTAINER_ID=$(docker run -d -p $local_port:$remote_port -v $local_lab_dir:$remote_lab_dir $docker_image)
 
 docker logs $CONTAINER_ID | sed -n 1p
 
+echo $local_port > $TOP_DIR/.lab_local_port
+
 which $browser 2>&1>/dev/null \
     && ($browser $url 2>&1>/dev/null &) \
-    && echo "please login with password: $pwd" && exit 0
+    && echo "please login $url with password: $pwd" && exit 0
 
 echo "Usage: Please open $url with password: $pwd"
